8e38752ccde4257c0525e7975defbd1fc519d3f4
juanmaruizf <juanmaruizf@gmail.com>
2021-01-07 16:28:14 +0000

    insertar encargos

--- app/controllers/crud_encargos_controller.rb
+++ app/controllers/crud_encargos_controller.rb
@@ -9,7 +9,31 @@ class CrudEncargosController < ApplicationController
     end
 
     def insertar
-        
+        @insertar = params[:nombreInput]
+        @nuevo
+        case insertar
+            when 0
+                @nuevo = Bizcocho.new(params[:nombre])
+                
+            when 1
+                @nuevo = Buttercream.new(params[:nombre])
+                
+            when 2
+                @nuevo = Galletum.new(params[:nombre])
+                
+            when 3
+                @nuevo = Popcake.new(params[:nombre])
+                
+            when 4
+                @nuevo = Mesadulce.new(params[:nombre])
+                
+            else 
+                
+        end
+        if @nuevo.save
+        else
+            render :new
+        end
     end
 
 
